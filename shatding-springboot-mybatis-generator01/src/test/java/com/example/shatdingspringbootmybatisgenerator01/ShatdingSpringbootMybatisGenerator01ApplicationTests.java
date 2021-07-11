57798e60026c1889855e84915ee2b35e8b9329a4
gwh <hang_15236270334@163.com>
2021-07-11 21:38:00 +0800

    shatding-jdbc 分库分表，读写分离测试代码

---
+++ shatding-springboot-mybatis-generator01/src/test/java/com/example/shatdingspringbootmybatisgenerator01/ShatdingSpringbootMybatisGenerator01ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.shatdingspringbootmybatisgenerator01;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShatdingSpringbootMybatisGenerator01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
